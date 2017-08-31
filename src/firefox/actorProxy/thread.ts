94df4b5c52428acf9d0a241d5b23996de4866333
Holger Benl <hbenl@evandor.de>
2017-08-31 18:26:46 +0200

    don't rely solely on newSource events

--- src/firefox/actorProxy/thread.ts
+++ src/firefox/actorProxy/thread.ts
@@ -277,6 +277,12 @@ export class ThreadActorProxy extends EventEmitter implements ActorProxy, IThrea
 			log.debug(`Received ${sources.length} sources from thread ${this.name}`);
 			this.pendingSourcesRequests.resolveOne(sources);
 
+			for (let source of sources) {
+				let sourceActor = this.connection.getOrCreate(source.actor, 
+					() => new SourceActorProxy(source, this.connection));
+				this.emit('newSource', sourceActor);
+			}
+
 		} else if (response['type'] === 'newSource') {
 			
 			let source = <FirefoxDebugProtocol.Source>(response['source']);
